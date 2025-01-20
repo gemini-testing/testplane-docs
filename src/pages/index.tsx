b0a7720b721c57767e05958c9e43df52370ecca5
shadowusr <nikhakerlab@gmail.com>
2025-01-20 23:34:58 +0300

    fix: fix landing page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import { LandingPage } from "@site/src/components/pages/LandingPage";
 
 export default function Home() {
-    const { siteConfig } = useDocusaurusContext();
-
     return (
         <Layout
-            title={siteConfig.title}
-            description="Testplane is a modern end-to-end testing framework"
+            title={"Fast, scalable and robust end-to-end web testing solution"}
+            description="A battle-hardened framework for creating tests at any scale and any browser. Get no-code, component, end-to-end and visual tests out of the box."
         >
             <LandingPage />
         </Layout>
