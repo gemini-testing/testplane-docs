ba96db5250e30957db4472fb82fd3c0f8e14e0a1
shadowusr <nikhakerlab@gmail.com>
2025-10-06 13:29:51 +0300

    fix: fix logo backgrounds

--- src/theme/Navbar/Logo/index.tsx
+++ src/theme/Navbar/Logo/index.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import TestplaneLogo from "@site/static/img/logo.svg";
 import TestplaneLogoDark from "@site/static/img/logo-dark.svg";
 import Link from "@docusaurus/Link";
+import clsx from "clsx";
+
+import styles from "./index.module.css";
 
 export default function NavbarLogo(): JSX.Element {
     return (
-        <div className="flex">
+        <div
+            className={clsx(
+                styles["logo-container"],
+                "logo-container flex bg-white/70 shadow-white/90 dark:bg-neutral-950/70 dark:shadow-neutral-950/90",
+            )}
+        >
             <TestplaneLogo className="h-8 w-8 dark:hidden" />
             <TestplaneLogoDark className="hidden h-8 w-8 dark:block" />
             <Link
