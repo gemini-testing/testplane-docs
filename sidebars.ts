d3468489076aa5d47591040efc79bbc1d37a6b2f
shadowusr <nikhakerlab@gmail.com>
2025-11-26 00:52:30 +0300

    docs: update docs on plugin development

--- sidebars.ts
+++ sidebars.ts
@@ -88,6 +88,7 @@ const sidebars: SidebarsConfig = {
             items: [
                 { type: "doc", id: "html-reporter/html-reporter-api" },
                 { type: "doc", id: "html-reporter/html-reporter-events" },
+                { type: "doc", id: "html-reporter/html-reporter-plugins-api" },
             ],
         },
     ],
